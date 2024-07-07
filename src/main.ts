eb82b9e343fb02e1ad5ffe692d26406bf8c45c6f
Juan Pablo Vargas <jupavar@gmail.com>
2024-07-07 13:17:05 -0300

    fix: add env variables

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('APP_PORT') ?? 3001;
+  await app.listen(port);
+  console.log('App listening port', port);
 }
 bootstrap();
